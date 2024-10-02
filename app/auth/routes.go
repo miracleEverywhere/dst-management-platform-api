c9bd948b94409e4a1670311d57f83e710ff2d4d8
Miracle <763483966@qq.com>
2024-10-02 17:02:46 +0800

    重构房间设置

--- app/auth/routes.go
+++ app/auth/routes.go
@@ -15,14 +15,8 @@ func RouteAuth(r *gin.Engine) *gin.Engine {
 		v1.POST("/update/password", utils.MWtoken(), handleUpdatePassword)
 
 		// 设置
-		v1.GET("/setting/room/base", utils.MWtoken(), handleRoomSettingBaseGet)
-		v1.POST("/setting/room/base", utils.MWtoken(), handleRoomSettingBasePost)
-		v1.GET("/setting/room/ground", utils.MWtoken(), handleRoomSettingGroundGet)
-		v1.POST("/setting/room/ground", utils.MWtoken(), handleRoomSettingGroundPost)
-		v1.GET("/setting/room/cave", utils.MWtoken(), handleRoomSettingCaveGet)
-		v1.POST("/setting/room/cave", utils.MWtoken(), handleRoomSettingCavePost)
-		v1.GET("/setting/room/mod", utils.MWtoken(), handleRoomSettingModGet)
-		v1.POST("/setting/room/mod", utils.MWtoken(), handleRoomSettingModPost)
+		v1.GET("/setting/room", utils.MWtoken(), handleRoomSettingGet)
+		v1.POST("/setting/room", utils.MWtoken(), handleRoomSettingPost)
 	}
 
 	return r
