50272038cfec3400df2752a2ed509c6beec95af6
Miracle <763483966@qq.com>
2024-11-28 09:58:47 +0800

    更新install脚本

--- utils/install.go
+++ utils/install.go
@@ -25,10 +25,10 @@ function check_directory() {
 }
 
 function install_ubuntu() {
+	apt update
     dpkg --add-architecture i386
-    apt update
     apt install -y lib32gcc1     
-	install -y lib32gcc-s1
+	apt install -y lib32gcc-s1
     apt install -y libcurl4-gnutls-dev:i386
     apt install -y screen
 	apt install -y unzip
