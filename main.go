9a0abd325655a2afcdf0b4008ac7cea0c0d0ab02
Miracle <763483966@qq.com>
2024-12-08 01:39:06 +0800

    优化main

--- main.go
+++ main.go
@@ -20,33 +20,23 @@ import (
 
 const VERSION string = "1.0.6"
 
-var (
-	// flag绑定的变量
-	bindPort      int
-	consoleOutput bool
-	versionShow   bool
-)
-
 //go:embed dist
 var EmbedFS embed.FS
 
 func main() {
 	//一些启动前检查
 	initialize()
-
-	if !consoleOutput {
+	flag.Parse()
+	if !utils.ConsoleOutput {
 		gin.DefaultWriter = io.Discard
 	}
-	if versionShow {
+	if utils.VersionShow {
 		fmt.Println(VERSION + "\n" + runtime.Version())
 		return
 	}
-
 	r := gin.Default()
-
 	//全局中间件，获取语言
 	r.Use(utils.MWlang())
-
 	//用户、鉴权模块
 	r = auth.RouteAuth(r)
 	//主页模块
@@ -59,12 +49,11 @@ func main() {
 	r = logs.RouteLogs(r)
 	// 外部接口
 	r = externalApi.RouteExternalApi(r)
-
 	//静态资源，放在最后
 	r.Use(static.ServeEmbed("dist", EmbedFS))
 
 	// 启动服务器
-	err := r.Run(fmt.Sprintf(":%d", bindPort))
+	err := r.Run(fmt.Sprintf(":%d", utils.BindPort))
 	if err != nil {
 		utils.Logger.Error("启动服务器失败", "err", err)
 		panic(err)
@@ -72,22 +61,15 @@ func main() {
 }
 
 func initialize() {
-	flag.IntVar(&bindPort, "l", 80, "监听端口，如： -l 8080 (Listening Port, e.g. -l 8080)")
-	flag.StringVar(&utils.ConfDir, "s", "./", "数据库文件目录，如： -s ./conf (Database Directory, e.g. -s ./conf)")
-	flag.BoolVar(&consoleOutput, "c", false, "开启控制台日志输出，如： -c (Enable console log output, e.g. -c)")
-	flag.BoolVar(&versionShow, "v", false, "查看版本，如： -v (Check version, e.g. -v)")
-	flag.Parse()
-
+	//绑定flag
+	utils.BindFlags()
 	//数据库检查
 	utils.CreateConfig()
-
 	//创建DST手动安装脚本
 	utils.CreateManualInstallScript()
-
 	//gin.SetMode(gin.DebugMode)
 	gin.SetMode(gin.ReleaseMode)
 	gin.DisableConsoleColor()
-
 	//加载定时任务
 	scheduler.InitTasks()
 	//启动定时任务调度器
