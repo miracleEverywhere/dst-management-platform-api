d30d462ee50cc443e4cb9c596afe3b0940041907
稚未狸 <1330757852@qq.com>
2024-12-09 18:18:14 +0800

    修改run.sh，添加手动构建配置文件
    * 构建时添加时区Asia/Shanghai
    
    * 修改dmp.tgz下载链接为GitHub与Gitee
    
    * 修改时区
    
    * 修改时区，安装tzdata
    
    * 时区设置改为在容器启动时进行校验
    
    * /config目录改为新建空目录
    
    * 添加手动构建配置文件
    
    * 修改手动actions文件
    
    * 修改手动actions文件
    
    * 删除分支中的手动构建

--- docker/entry-point.sh
+++ docker/entry-point.sh
@@ -5,7 +5,13 @@ DMP_HOME="/root"
 
 # 由于install.go里没有安装wget，在启动容器的时候安装；或者在install.go进行安装
 apt update
-apt install -y wget unzip
+apt install -y wget unzip systemd
+
+# 从环境变量中获取TZ设置
+TZ=$(env | grep TZ | cut -d '=' -f 2)
+# 设置时区
+echo "Setting timezone to $TZ..."
+timedatectl set-timezone "$TZ" || { echo "Error: Failed to set timezone."; exit 1; }
 
 cd $DMP_HOME || exit
 # shellcheck disable=SC2069
