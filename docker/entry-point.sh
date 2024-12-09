c1bf3714037bba0b46e8674bdfc40029fab723fa
harelive <1330757852@qq.com>
2024-12-09 23:19:37 +0800

    修改时间校验依赖包为tzdata

--- docker/entry-point.sh
+++ docker/entry-point.sh
@@ -5,13 +5,13 @@ DMP_HOME="/root"
 
 # 由于install.go里没有安装wget，在启动容器的时候安装；或者在install.go进行安装
 apt update
-apt install -y wget unzip systemd
+apt install -y wget unzip tzdata
 
 # 从环境变量中获取TZ设置
 TZ=$(env | grep TZ | cut -d '=' -f 2)
 # 设置时区
 echo "Setting timezone to $TZ..."
-timedatectl set-timezone "$TZ" || { echo "Error: Failed to set timezone."; exit 1; }
+ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo '$TZ' > /etc/timezone
 
 cd $DMP_HOME || exit
 # shellcheck disable=SC2069
