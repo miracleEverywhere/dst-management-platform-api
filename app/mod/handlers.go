7506db49c1b11aaed4adde6e993e8da67441c289
Miracle <763483966@qq.com>
2024-12-12 16:50:08 +0800

    更新mod配置获取接口

--- app/mod/handlers.go
+++ app/mod/handlers.go
@@ -59,6 +59,15 @@ func handleModSettingFormatGet(c *gin.Context) {
 }
 
 func handleModConfigOptionsGet(c *gin.Context) {
+	type ModConfigurationsForm struct {
+		ID int `form:"id" json:"id"`
+	}
+	var modConfigurationsForm ModConfigurationsForm
+	if err := c.ShouldBindQuery(&modConfigurationsForm); err != nil {
+		// 如果绑定失败，返回 400 错误
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
 	lang, _ := c.Get("lang")
 	langStr := "zh" // 默认语言
 	if strLang, ok := lang.(string); ok {
@@ -69,7 +78,7 @@ func handleModConfigOptionsGet(c *gin.Context) {
 		ConfigOptions []utils.ConfigurationOption `json:"configOptions"`
 	}
 	var modConfig ModConfig
-	modID := 1216718131
+	modID := modConfigurationsForm.ID
 	modInfoLuaFile := utils.ModUgcPath + "/" + strconv.Itoa(modID) + "/modinfo.lua"
 	isUgcMod, err := utils.FileDirectoryExists(modInfoLuaFile)
 	if err != nil {
@@ -79,6 +88,15 @@ func handleModConfigOptionsGet(c *gin.Context) {
 
 	if !isUgcMod {
 		modInfoLuaFile = utils.ModNoUgcPath + "/workshop-" + strconv.Itoa(modID) + "/modinfo.lua"
+		exist, err := utils.FileDirectoryExists(modInfoLuaFile)
+		if err != nil {
+			utils.RespondWithError(c, 500, langStr)
+			return
+		}
+		if !exist {
+			c.JSON(http.StatusOK, gin.H{"code": 200, "message": response("needDownload", langStr), "data": nil})
+			return
+		}
 	}
 
 	luaScript, _ := utils.GetFileAllContent(modInfoLuaFile)
--- app/mod/i18n.go
+++ app/mod/i18n.go
@@ -1,1 +1,16 @@
 package mod
+
+func response(message string, lang string) string {
+	responseZH := map[string]string{
+		"needDownload": "请先下载模组",
+	}
+	responseEN := map[string]string{
+		"needDownload": "Please download MOD first",
+	}
+
+	if lang == "zh" {
+		return responseZH[message]
+	} else {
+		return responseEN[message]
+	}
+}
