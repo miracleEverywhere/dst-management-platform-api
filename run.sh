729bcd54e3dfb13ff04a4bb1aa42c8a5e320548f
ssxzi <42813459+ssxzi@users.noreply.github.com>
2025-01-06 14:06:29 +0800

    Update run.sh
    - 支持动态指定端口运行脚本
    - 更新服务增加版本号对比
    - 添加强制更新功能

--- run.sh
+++ run.sh
@@ -29,6 +29,7 @@ function prompt_user() {
     echo -e "\e[32m[2]: 关闭服务(Stop the service) \e[0m"
     echo -e "\e[32m[3]: 重启服务(Restart the service) \e[0m"
     echo -e "\e[32m[4]: 更新服务(Update the service) \e[0m"
+    echo -e "\e[32m[5]: 强制更新(Mandatory update) \e[0m"
 }
 
 # 检查jq
@@ -130,6 +131,8 @@ function check_dmp() {
 
 # 启动主程序
 function start_dmp() {
+    echo -e "\e[33m请输入dmp暴露端口，即网页打开时所用的端口 (Please enter the port for dmp, which is the port used to access the webpage): \e[0m"
+    read -r PORT
     if [ -e "$ExeFile" ]; then
         nohup "$ExeFile" -c -l ${PORT} -s ${CONFIG_DIR} >dmp.log 2>&1 &
     else
@@ -149,6 +152,24 @@ function stop_dmp() {
 function clear_dmp() {
     echo -e "\e[36m正在执行清理 (Cleaning Files) \e[0m"
     rm -f dmp*
+}
+
+# 检查当前版本号
+function get_current_version() {
+    if [ -e "$ExeFile" ]; then
+        CURRENT_VERSION=$("$ExeFile" -v | head -n1) # 获取输出的第一行作为版本号
+    else
+        CURRENT_VERSION="0.0.0"
+    fi
+}
+
+# 获取GitHub最新版本号
+function get_latest_version() {
+    LATEST_VERSION=$(curl -s https://api.github.com/repos/miracleEverywhere/dst-management-platform-api/releases/latest | jq -r .tag_name | grep -oP '(\d+\.)+\d+')
+    if [[ -z "$LATEST_VERSION" ]]; then
+        echo -e "\e[31m无法获取最新版本号，请检查网络连接或GitHub API (Failed to fetch the latest version, please check network or GitHub API) \e[0m"
+        exit 1
+    fi
 }
 
 # 使用无限循环让用户输入命令
@@ -185,18 +206,35 @@ while true; do
         echo -e "\e[32m重启成功 (Restart Success) \e[0m"
         break
         ;;
-    4)
+	 4)
+		get_current_version
+		get_latest_version
+		if [[ "$(echo -e "$CURRENT_VERSION\n$LATEST_VERSION" | sort -V | head -n1)" == "$CURRENT_VERSION" && "$CURRENT_VERSION" != "$LATEST_VERSION" ]]; then
+			echo -e "\e[33m当前版本 ($CURRENT_VERSION) 小于最新版本 ($LATEST_VERSION)，即将更新 (Updating to the latest version) \e[0m"
+			stop_dmp
+			clear_dmp
+			check_glibc
+			install_dmp
+			start_dmp
+			check_dmp
+			echo -e "\e[32m更新完成 (Update completed) \e[0m"
+		else
+			echo -e "\e[32m当前版本 ($CURRENT_VERSION) 已是最新版本 ($LATEST_VERSION)，无需更新 (No update needed) \e[0m"
+		fi
+		break
+		;;
+    5)
         stop_dmp
         clear_dmp
         check_glibc
         install_dmp
         start_dmp
         check_dmp
-        echo -e "\e[32m更新成功 (Restart Success) \e[0m"
+        echo -e "\e[32m重装完成 (Reinstallation completed) \e[0m"
         break
         ;;
     *)
-        echo -e "\e[31m无效输入，请输入 0, 1, 2, 3, 4 (Invalid input, please enter 0, 1, 2, 3, 4) \e[0m"
+        echo -e "\e[31m无效输入，请输入 0, 1, 2, 3, 4, 5 (Invalid input, please enter 0, 1, 2, 3, 4, 5) \e[0m"
         continue
         ;;
     esac
