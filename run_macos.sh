effe1dadb607a5926792ebfbff1d264462971ed9
Miracle <763483966@qq.com>
2025-01-17 16:42:21 +0800

    优化启动脚本

--- run_macos.sh
+++ run_macos.sh
@@ -28,7 +28,6 @@ function prompt_user() {
     echo -e "\e[32m[2]: 关闭服务(Stop the service) \e[0m"
     echo -e "\e[32m[3]: 重启服务(Restart the service) \e[0m"
     echo -e "\e[32m[4]: 更新服务(Update the service) \e[0m"
-    echo -e "\e[32m[5]: 强制更新(Mandatory update) \e[0m"
 }
 
 # 检查jq
@@ -70,7 +69,7 @@ function install_dmp() {
     check_curl
     check_wget
     # 原GitHub下载链接
-    GITHUB_URL=$(curl -s https://api.github.com/repos/miracleEverywhere/dst-management-platform-api/releases/latest | jq -r .assets[1].browser_download_url)
+    GITHUB_URL=$(curl -s https://api.github.com/repos/miracleEverywhere/dst-management-platform-api/releases/latest | jq -r ".assets[1].browser_download_url")
     # 加速站点，失效从 https://github.akams.cn/ 重新搜索。
     PRIMARY_PROXY="https://ghproxy.cc/"   # 主加速站点
     SECONDARY_PROXY="https://ghproxy.cn/" # 备用加速站点
@@ -88,7 +87,7 @@ function install_dmp() {
         else
             echo -e "\e[31m备用加速站点下载失败: wget 返回码为 $?, 尝试从 Gitee 下载\e[0m"
             # Gitee下载链接
-            GITEE_URL=$(curl -s https://gitee.com/api/v5/repos/s763483966/dst-management-platform-api/releases/latest | jq -r .assets[1].browser_download_url)
+            GITEE_URL=$(curl -s https://gitee.com/api/v5/repos/s763483966/dst-management-platform-api/releases/latest | jq -r ".assets[1].browser_download_url")
             # 尝试从 Gitee 下载
             echo -e "\e[36m尝试通过国内站点下载 Gitee\e[0m"
             if download "$GITEE_URL" 5 10; then
@@ -146,26 +145,6 @@ function clear_dmp() {
     rm -f dmp*
 }
 
-# 检查当前版本号
-function get_current_version() {
-    if [ -e "$ExeFile" ]; then
-        CURRENT_VERSION=$("$ExeFile" -v | head -n1) # 获取输出的第一行作为版本号
-    else
-        CURRENT_VERSION="0.0.0"
-    fi
-}
-
-# 获取GitHub最新版本号
-function get_latest_version() {
-    check_curl
-    check_jq
-    LATEST_VERSION=$(curl -s https://api.github.com/repos/miracleEverywhere/dst-management-platform-api/releases/latest | jq -r .tag_name | grep -oP '(\d+\.)+\d+')
-    if [[ -z "$LATEST_VERSION" ]]; then
-        echo -e "\e[31m无法获取最新版本号，请检查网络连接或GitHub API (Failed to fetch the latest version, please check network or GitHub API) \e[0m"
-        exit 1
-    fi
-}
-
 
 # 使用无限循环让用户输入命令
 while true; do
@@ -199,32 +178,16 @@ while true; do
         break
         ;;
     4)
-        get_current_version
-        get_latest_version
-        if [[ "$(echo -e "$CURRENT_VERSION\n$LATEST_VERSION" | sort -V | head -n1)" == "$CURRENT_VERSION" && "$CURRENT_VERSION" != "$LATEST_VERSION" ]]; then
-            echo -e "\e[33m当前版本 ($CURRENT_VERSION) 小于最新版本 ($LATEST_VERSION)，即将更新 (Updating to the latest version) \e[0m"
-            stop_dmp
-            clear_dmp
-            install_dmp
-            start_dmp
-            check_dmp
-            echo -e "\e[32m更新完成 (Update completed) \e[0m"
-        else
-            echo -e "\e[32m当前版本 ($CURRENT_VERSION) 已是最新版本 ($LATEST_VERSION)，无需更新 (No update needed) \e[0m"
-        fi
-        break
-        ;;
-    5)
         stop_dmp
         clear_dmp
         install_dmp
         start_dmp
         check_dmp
-        echo -e "\e[32m强制更新完成 (Force update completed) \e[0m"
+        echo -e "\e[32m更新完成 (Update completed) \e[0m"
         break
         ;;
     *)
-        echo -e "\e[31m无效输入，请输入 0, 1, 2, 3, 4, 5 (Invalid input, please enter 0, 1, 2, 3, 4, 5) \e[0m"
+        echo -e "\e[31m无效输入，请输入 0, 1, 2, 3, 4 (Invalid input, please enter 0, 1, 2, 3, 4) \e[0m"
         continue
         ;;
     esac
