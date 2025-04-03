2b14759dedf4c218570e5e10eab13e0fbfaf944b
Miracle <763483966@qq.com>
2025-04-03 15:30:29 +0800

    登录接口进行ip限流

--- app/auth/routes.go
+++ app/auth/routes.go
@@ -3,13 +3,17 @@ package auth
 import (
 	"dst-management-platform-api/utils"
 	"github.com/gin-gonic/gin"
+	"time"
 )
 
 func RouteAuth(r *gin.Engine) *gin.Engine {
+	// 同一个IP每分钟最多登录3次
+	ipLimiter := utils.NewIPRateLimiter(3, time.Minute)
+	defer ipLimiter.Stop()
 	v1 := r.Group("v1")
 	{
 		// 系统
-		v1.POST("/login", handleLogin)
+		v1.POST("/login", ipLimiter.MWIPLimiter(), handleLogin)
 		v1.GET("/userinfo", utils.MWtoken(), handleUserinfo)
 		v1.GET("/menu", utils.MWtoken(), handleMenu)
 		v1.POST("/update/password", utils.MWtoken(), handleUpdatePassword)
