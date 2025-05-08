057f083dadb151ad4abbadacb2da3d79f3f3d891
Miracle <763483966@qq.com>
2025-05-08 17:54:28 +0800

    新增玩家更新mod全局变量

--- utils/cache.go
+++ utils/cache.go
@@ -1,10 +1,12 @@
 package utils
 
 var (
-	Platform   string
-	Registered bool
-	HomeDir    string
-	STATISTICS = make(map[string][]Statistics) // 玩家统计
-	SYSMETRICS []SysMetrics                    // 系统监控
-	UserCache  = make(map[string]User)
+	Platform            string
+	Registered          bool
+	HomeDir             string
+	STATISTICS          = make(map[string][]Statistics) // 玩家统计
+	SYSMETRICS          []SysMetrics                    // 系统监控
+	UserCache           = make(map[string]User)
+	UpdateModID         string // 玩家通过聊天窗口进行mod更新的校验ID
+	UpdateModProcessing bool   // 确定更新后会sleep一段时间，该值为true
 )
