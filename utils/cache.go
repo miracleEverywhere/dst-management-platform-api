adc98f6f05d4599bd465c8ad03e181fab81a218c
miracleEverywhere <763483966@qq.com>
2025-06-28 18:28:25 +0800

    添加玩家游戏时长统计变量

--- utils/cache.go
+++ utils/cache.go
@@ -6,6 +6,7 @@ var (
 	HomeDir             string
 	STATISTICS          = make(map[string][]Statistics) // 玩家统计
 	SYSMETRICS          []SysMetrics                    // 系统监控
+	PlayTimeCount       = make(map[string]map[string]int64)
 	UserCache           = make(map[string]User)
 	UpdateModProcessing bool // 确定更新后会sleep一段时间，该值为true
 	InContainer         bool
