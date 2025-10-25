abb56ecfa103acc9b0655404a23a059e9015dca7
miracleEverywhere <763483966@qq.com>
2025-10-25 23:10:17 +0800

    v3.0.0 初始化

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module dst-management-platform-api
 go 1.23.1
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.11.0
 	gorm.io/driver/sqlite v1.6.0
 	gorm.io/gorm v1.31.0
--- utils/jwt.go
+++ utils/jwt.go
@@ -2,41 +2,42 @@ package utils
 
 import (
 	"dst-management-platform-api/models"
+	"github.com/dgrijalva/jwt-go"
 	"math/rand"
 	"time"
 )
 
 func GenerateJWTSecret() string {
-    source := rand.NewSource(time.Now().UnixNano())
-    r := rand.New(source)
-    charset := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-    length := 26
-    randomString := make([]byte, length)
-    for i := range randomString {
-        // 从字符集中随机选择一个字符
-        randomString[i] = charset[r.Intn(len(charset))]
-    }
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+	charset := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	length := 26
+	randomString := make([]byte, length)
+	for i := range randomString {
+		// 从字符集中随机选择一个字符
+		randomString[i] = charset[r.Intn(len(charset))]
+	}
 
-    return string(randomString)
+	return string(randomString)
 }
 
 func GenerateJWT(user models.User, jwtSecret []byte, expiration int) (string, error) {
-    type Claims struct {
-        Username string `json:"username"`
-        Nickname string `json:"nickname"`
-        Role     string `json:"role"`
-        jwt.StandardClaims
-    }
+	type Claims struct {
+		Username string `json:"username"`
+		Nickname string `json:"nickname"`
+		Role     string `json:"role"`
+		jwt.StandardClaims
+	}
 
-    claims := Claims{
-        Username: user.Username,
-        Nickname: user.Nickname,
-        Role:     user.Role,
-        StandardClaims: jwt.StandardClaims{
-            ExpiresAt: time.Now().Add(time.Duration(expiration) * time.Hour).Unix(), // 过期时间
-        },
-    }
+	claims := Claims{
+		Username: user.Username,
+		Nickname: user.Nickname,
+		Role:     user.Role,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(time.Duration(expiration) * time.Hour).Unix(), // 过期时间
+		},
+	}
 
-    token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-    return token.SignedString(jwtSecret)
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(jwtSecret)
 }
