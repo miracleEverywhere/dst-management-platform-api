9b7c92e1ec9b79fc560d47e0c292b5cde1d5df96
miracleEverywhere <763483966@qq.com>
2025-10-25 23:54:18 +0800

    添加菜单

--- app/user/router.go
+++ app/user/router.go
@@ -1,17 +1,19 @@
 package user
 
 import (
-	"dst-management-platform-api/constants"
-	"github.com/gin-gonic/gin"
+    "dst-management-platform-api/constants"
+    "dst-management-platform-api/middleware"
+    "github.com/gin-gonic/gin"
 )
 
 func (h *Handler) RegisterRoutes(r *gin.Engine) {
-	v := r.Group(constants.ApiVersion)
-	{
-		auth := v.Group("user")
-		{
-			auth.POST("/register", h.registerPost)
-			auth.POST("/login", h.loginPost)
-		}
-	}
+    v := r.Group(constants.ApiVersion)
+    {
+        auth := v.Group("user")
+        {
+            auth.POST("/register", h.registerPost)
+            auth.POST("/login", h.loginPost)
+            auth.GET("/menu", middleware.MWtoken(), h.menuGet)
+        }
+    }
 }
