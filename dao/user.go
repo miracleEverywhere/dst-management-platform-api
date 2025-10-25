5cd7f31eebb6eb3ae15ba4c7379e8aa9c43f2e9d
miracleEverywhere <763483966@qq.com>
2025-10-25 23:10:17 +0800

    v3.0.0 初始化

--- dao/user.go
+++ dao/user.go
@@ -1,28 +1,32 @@
 package dao
 
 import (
-	"dst-management-platform-api/models"
-	"gorm.io/gorm"
+    "dst-management-platform-api/models"
+    "errors"
+    "gorm.io/gorm"
 )
 
 type UserDAO struct {
-	BaseDAO[models.User]
+    BaseDAO[models.User]
 }
 
 func NewUserDAO(db *gorm.DB) *UserDAO {
-	return &UserDAO{
-		BaseDAO: *NewBaseDAO[models.User](db),
-	}
+    return &UserDAO{
+        BaseDAO: *NewBaseDAO[models.User](db),
+    }
 }
 
 func (d *UserDAO) GetUserByUsername(username string) (*models.User, error) {
-	var user models.User
-	err := d.db.Where("username = ?", username).First(&user).Error
-	return &user, err
+    var user models.User
+    err := d.db.Where("username = ?", username).First(&user).Error
+    if errors.Is(err, gorm.ErrRecordNotFound) {
+        return &user, nil
+    }
+    return &user, err
 }
 
 func (d *UserDAO) FindByEmail(email string) (*models.User, error) {
-	var user models.User
-	err := d.db.Where("email = ?", email).First(&user).Error
-	return &user, err
+    var user models.User
+    err := d.db.Where("email = ?", email).First(&user).Error
+    return &user, err
 }
