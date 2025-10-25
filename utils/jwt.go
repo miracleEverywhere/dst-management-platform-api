b3f5052881f9ae31715f5bc0f20828828442c62a
miracleEverywhere <763483966@qq.com>
2025-10-25 23:54:34 +0800

    添加令牌中间件

--- utils/jwt.go
+++ utils/jwt.go
@@ -1,43 +1,62 @@
 package utils
 
 import (
-	"dst-management-platform-api/models"
-	"github.com/dgrijalva/jwt-go"
-	"math/rand"
-	"time"
+    "dst-management-platform-api/models"
+    "fmt"
+    "github.com/dgrijalva/jwt-go"
+    "math/rand"
+    "time"
 )
 
-func GenerateJWTSecret() string {
-	source := rand.NewSource(time.Now().UnixNano())
-	r := rand.New(source)
-	charset := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-	length := 26
-	randomString := make([]byte, length)
-	for i := range randomString {
-		// 从字符集中随机选择一个字符
-		randomString[i] = charset[r.Intn(len(charset))]
-	}
+type Claims struct {
+    Username string `json:"username"`
+    Nickname string `json:"nickname"`
+    Role     string `json:"role"`
+    jwt.StandardClaims
+}
 
-	return string(randomString)
+func GenerateJWTSecret() string {
+    source := rand.NewSource(time.Now().UnixNano())
+    r := rand.New(source)
+    charset := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    length := 26
+    randomString := make([]byte, length)
+    for i := range randomString {
+        // 从字符集中随机选择一个字符
+        randomString[i] = charset[r.Intn(len(charset))]
+    }
+
+    return string(randomString)
 }
 
 func GenerateJWT(user models.User, jwtSecret []byte, expiration int) (string, error) {
-	type Claims struct {
-		Username string `json:"username"`
-		Nickname string `json:"nickname"`
-		Role     string `json:"role"`
-		jwt.StandardClaims
-	}
 
-	claims := Claims{
-		Username: user.Username,
-		Nickname: user.Nickname,
-		Role:     user.Role,
-		StandardClaims: jwt.StandardClaims{
-			ExpiresAt: time.Now().Add(time.Duration(expiration) * time.Hour).Unix(), // 过期时间
-		},
-	}
+    claims := Claims{
+        Username: user.Username,
+        Nickname: user.Nickname,
+        Role:     user.Role,
+        StandardClaims: jwt.StandardClaims{
+            ExpiresAt: time.Now().Add(time.Duration(expiration) * time.Hour).Unix(), // 过期时间
+        },
+    }
 
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	return token.SignedString(jwtSecret)
+    token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+    return token.SignedString(jwtSecret)
+}
+
+func ValidateJWT(tokenString string, jwtSecret []byte) (*Claims, error) {
+    claims := &Claims{}
+    token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+        return jwtSecret, nil
+    })
+
+    if err != nil {
+        return nil, err
+    }
+
+    if claims, ok := token.Claims.(*Claims); ok && token.Valid {
+        return claims, nil
+    }
+
+    return nil, fmt.Errorf("invalid token")
 }
