af5273e6b6dd17bacd295a1c02dfdc715a51be89
miracleEverywhere <763483966@qq.com>
2025-10-26 18:57:06 +0800

    数据库

--- database/dao/world.go
+++ database/dao/world.go
@@ -2,6 +2,7 @@ package dao
 
 import (
 	"dst-management-platform-api/database/models"
+	"errors"
 	"gorm.io/gorm"
 )
 
@@ -13,4 +14,16 @@ func NewWorldDAO(db *gorm.DB) *WorldDAO {
 	return &WorldDAO{
 		BaseDAO: *NewBaseDAO[models.World](db),
 	}
+}
+
+func (d *WorldDAO) GetWorldsByFingerPrints(fingerPrints []string) ([]models.World, error) {
+	worlds, err := d.Query("world_finger_print IN ?", fingerPrints)
+	if err != nil {
+		return nil, err
+	}
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		return worlds, nil
+	}
+
+	return worlds, err
 }
