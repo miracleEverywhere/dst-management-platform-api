77e8cbe55e0cdb123755fa83d10e0b6492d43ef2
miracleEverywhere <763483966@qq.com>
2025-10-27 18:26:10 +0800

    修改表结构

--- database/models/user.go
+++ database/models/user.go
@@ -4,9 +4,9 @@ type User struct {
 	Username     string `gorm:"primaryKey;not null" json:"username"`
 	Nickname     string `gorm:"not null" json:"nickname"`
 	Role         string `gorm:"not null" json:"role"`
+	Avatar       string `gorm:"not null" json:"avatar"`
 	Password     string `gorm:"not null" json:"password"`
 	Disabled     bool   `gorm:"not null" json:"disabled"`
-	Menus        string `json:"menus"`
 	Rooms        string `json:"rooms"`
 	RoomCreation bool   `gorm:"not null" json:"roomCreation"`
 	MaxWorlds    int    `gorm:"not null" json:"maxWorlds"`
