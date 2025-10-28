e7df571738fd5ed773cff381c746261e498d7ef9
miracleEverywhere <763483966@qq.com>
2025-10-28 11:56:55 +0800

    修改i18n

--- app/user/handler.go
+++ app/user/handler.go
@@ -26,20 +26,20 @@ func (h *Handler) registerPost(c *gin.Context) {
 	var user models.User
 	if err := c.ShouldBindJSON(&user); err != nil {
 		logger.Logger.Info("请求参数错误", "err", err, "api", c.Request.URL.Path)
-		c.JSON(http.StatusOK, gin.H{"code": 400, "message": message(c, "bad request"), "data": nil})
+		c.JSON(http.StatusOK, gin.H{"code": 400, "message": message.Get(c, "bad request"), "data": nil})
 		return
 	}
 
 	num, err := h.userDao.Count(nil)
 	if err != nil {
 		logger.Logger.Error("查询数据库失败", "err", err)
-		c.JSON(http.StatusOK, gin.H{"code": 500, "message": message(c, "register fail"), "data": nil})
+		c.JSON(http.StatusOK, gin.H{"code": 500, "message": message.Get(c, "register fail"), "data": nil})
 		return
 	}
 
 	if num != 0 {
 		logger.Logger.Info("创建用户失败，用户已存在", "err", err)
-		c.JSON(http.StatusOK, gin.H{"code": 201, "message": message(c, "user exist"), "data": nil})
+		c.JSON(http.StatusOK, gin.H{"code": 201, "message": message.Get(c, "user exist"), "data": nil})
 		return
 	}
 
@@ -48,11 +48,11 @@ func (h *Handler) registerPost(c *gin.Context) {
 
 	if errCreate := h.userDao.Create(&user); errCreate != nil {
 		logger.Logger.Error("创建用户失败", "err", errCreate)
-		c.JSON(http.StatusOK, gin.H{"code": 500, "message": message(c, "register fail"), "data": nil})
+		c.JSON(http.StatusOK, gin.H{"code": 500, "message": message.Get(c, "register fail"), "data": nil})
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"code": 200, "message": message(c, "register success"), "data": nil})
+	c.JSON(http.StatusOK, gin.H{"code": 200, "message": message.Get(c, "register success"), "data": nil})
 	return
 }
 
@@ -60,45 +60,45 @@ func (h *Handler) loginPost(c *gin.Context) {
 	var user models.User
 	if err := c.ShouldBindJSON(&user); err != nil {
 		logger.Logger.Info("请求参数错误", "err", err, "api", c.Request.URL.Path)
-		c.JSON(http.StatusOK, gin.H{"code": 400, "message": message(c, "bad request"), "data": nil})
+		c.JSON(http.StatusOK, gin.H{"code": 400, "message": message.Get(c, "bad request"), "data": nil})
 		return
 	}
 	if user.Username == "" || user.Password == "" {
 		logger.Logger.Info("请求参数缺失", "api", c.Request.URL.Path)
-		c.JSON(http.StatusOK, gin.H{"code": 400, "message": message(c, "bad request"), "data": nil})
+		c.JSON(http.StatusOK, gin.H{"code": 400, "message": message.Get(c, "bad request"), "data": nil})
 		return
 	}
 
 	dbUser, err := h.userDao.GetUserByUsername(user.Username)
 	if err != nil {
 		logger.Logger.Error("查询数据库失败", "err", err)
-		c.JSON(http.StatusOK, gin.H{"code": 500, "message": message(c, "login fail"), "data": nil})
+		c.JSON(http.StatusOK, gin.H{"code": 500, "message": message.Get(c, "login fail"), "data": nil})
 		return
 	}
 
 	if dbUser.Username == "" {
-		c.JSON(http.StatusOK, gin.H{"code": 201, "message": message(c, "user not exist"), "data": nil})
+		c.JSON(http.StatusOK, gin.H{"code": 201, "message": message.Get(c, "user not exist"), "data": nil})
 		return
 	}
 
 	if dbUser.Disabled {
-		c.JSON(http.StatusOK, gin.H{"code": 201, "message": message(c, "disabled"), "data": nil})
+		c.JSON(http.StatusOK, gin.H{"code": 201, "message": message.Get(c, "disabled"), "data": nil})
 		return
 	}
 
 	if dbUser.Password != user.Password {
-		c.JSON(http.StatusOK, gin.H{"code": 201, "message": message(c, "wrong password"), "data": nil})
+		c.JSON(http.StatusOK, gin.H{"code": 201, "message": message.Get(c, "wrong password"), "data": nil})
 		return
 	}
 
 	token, err := utils.GenerateJWT(*dbUser, []byte(db.JwtSecret), 24)
 	if err != nil {
 		logger.Logger.Error("生成jwt失败", "err", err)
-		c.JSON(http.StatusOK, gin.H{"code": 500, "message": message(c, "login fail"), "data": nil})
+		c.JSON(http.StatusOK, gin.H{"code": 500, "message": message.Get(c, "login fail"), "data": nil})
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"code": 200, "message": message(c, "login success"), "data": token})
+	c.JSON(http.StatusOK, gin.H{"code": 200, "message": message.Get(c, "login success"), "data": token})
 }
 
 func (h *Handler) menuGet(c *gin.Context) {
@@ -153,7 +153,7 @@ func (h *Handler) userInfo(c *gin.Context) {
 	dbUser, err := h.userDao.GetUserByUsername(username.(string))
 	if err != nil {
 		logger.Logger.Error("查询数据库失败", "err", err)
-		c.JSON(http.StatusOK, gin.H{"code": 500, "message": message(c, "login fail"), "data": nil})
+		c.JSON(http.StatusOK, gin.H{"code": 500, "message": message.Get(c, "login fail"), "data": nil})
 		return
 	}
 	dbUser.Password = ""
