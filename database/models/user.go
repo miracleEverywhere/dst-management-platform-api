772875c265b609baece57680ba54d606d921e605
miracleEverywhere <763483966@qq.com>
2025-10-29 23:30:59 +0800

    唉

--- database/models/user.go
+++ database/models/user.go
@@ -1,15 +1,16 @@
 package models
 
 type User struct {
-	Username     string `gorm:"primaryKey;not null" json:"username"`
-	Nickname     string `gorm:"not null" json:"nickname"`
-	Role         string `gorm:"not null" json:"role"`
-	Avatar       string `gorm:"not null" json:"avatar"`
-	Password     string `gorm:"not null" json:"password"`
-	Disabled     bool   `gorm:"not null" json:"disabled"`
-	Rooms        string `json:"rooms"`
-	RoomCreation bool   `gorm:"not null" json:"roomCreation"`
-	MaxWorlds    int    `gorm:"not null" json:"maxWorlds"`
+	Username      string `gorm:"primaryKey;not null" json:"username"`
+	Nickname      string `gorm:"not null" json:"nickname"`
+	Role          string `gorm:"not null" json:"role"`
+	Avatar        string `gorm:"not null" json:"avatar"`
+	Password      string `gorm:"not null" json:"password"`
+	Disabled      bool   `gorm:"not null" json:"disabled"`
+	Rooms         string `json:"rooms"`
+	RoomCreation  bool   `gorm:"not null" json:"roomCreation"`
+	MaxWorlds     int    `gorm:"not null" json:"maxWorlds"`
+	CustomSetting string `json:"customSetting"`
 }
 
 func (User) TableName() string {
