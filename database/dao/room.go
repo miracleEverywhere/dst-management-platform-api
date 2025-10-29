33790450d54774ef9fd4d9af1b2925aabf1472b2
miracleEverywhere <763483966@qq.com>
2025-10-29 17:26:28 +0800

    世界恶堕中。。。

--- database/dao/room.go
+++ database/dao/room.go
@@ -2,8 +2,10 @@ package dao
 
 import (
 	"dst-management-platform-api/database/models"
+	"dst-management-platform-api/logger"
 	"errors"
 	"gorm.io/gorm"
+	"strings"
 )
 
 type RoomDAO struct {
@@ -55,4 +57,96 @@ func (d *RoomDAO) ListRooms(roomNames []string, roomName string, page, pageSize 
 
 	rooms, err := d.Query(page, pageSize, condition, args...)
 	return rooms, err
+}
+
+func (d *RoomDAO) DeleteRoomByName(name string) error {
+	// 开始事务
+	tx := d.db.Begin()
+	if tx.Error != nil {
+		return tx.Error
+	}
+
+	defer func() {
+		if r := recover(); r != nil {
+			tx.Rollback()
+			logger.Logger.Error("回滚事务失败", "panic", r)
+		}
+	}()
+
+	// 删除rooms表中的数据
+	if err := tx.Where("name = ?", name).Delete(&models.Room{}).Error; err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	// 删除worlds表中的数据
+	if err := tx.Where("room_name = ?", name).Delete(&models.World{}).Error; err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	// 删除room_settings表中的数据
+	if err := tx.Where("room_name = ?", name).Delete(&models.World{}).Error; err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	// 更新users表中的rooms权限
+	if err := d.updateUserRooms(tx, name); err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	// 提交事务
+	if err := tx.Commit().Error; err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	return nil
+}
+
+// updateUserRooms 更新用户 rooms 字段，删除指定的 roomName
+func (d *RoomDAO) updateUserRooms(tx *gorm.DB, roomName string) error {
+	// 查询所有包含该 roomName 的用户
+	var users []models.User
+	searchPattern := "%" + roomName + "%"
+
+	if err := tx.Where("rooms LIKE ?", searchPattern).Find(&users).Error; err != nil {
+		return err
+	}
+
+	for _, user := range users {
+		if user.Rooms == "" {
+			continue
+		}
+
+		// 分割 rooms 字符串
+		rooms := strings.Split(user.Rooms, ",")
+
+		// 过滤掉要删除的 roomName
+		var newRooms []string
+		for _, room := range rooms {
+			if strings.TrimSpace(room) != roomName {
+				newRooms = append(newRooms, room)
+			}
+		}
+
+		// 重新组合 rooms 字符串
+		newRoomsStr := strings.Join(newRooms, ",")
+
+		// 如果 rooms 字段为空，可以设置为空字符串或 NULL
+		if newRoomsStr == "" {
+			newRoomsStr = ""
+		}
+
+		// 更新用户记录
+		if err := tx.Model(&models.User{}).
+			Where("username = ?", user.Username).
+			Update("rooms", newRoomsStr).Error; err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
