a03c094ba43228052e7a808cb4b18d1c91d8f7f0
miracleEverywhere <763483966@qq.com>
2025-10-31 17:09:05 +0800

    周五咯

--- database/models/room.go
+++ database/models/room.go
@@ -1,24 +1,25 @@
 package models
 
 type Room struct {
-	Name              string `gorm:"primaryKey;not null" json:"name"`
-	DisplayName       string `json:"displayName"`
-	Status            bool   `json:"status"`
-	GameName          string `json:"gameName"`
-	Description       string `json:"description"`
-	GameMode          string `json:"gameMode"`
-	CustomGameMode    string `json:"customGameMode"`
-	Pvp               bool   `json:"pvp"`
-	MaxPlayer         int    `json:"maxPlayer"`
-	MaxRollBack       int    `json:"maxRollBack"`
-	Vote              bool   `json:"vote"`
-	PauseEmpty        bool   `json:"pauseEmpty"`
-	Password          string `json:"password"`
-	Token             string `json:"token"`
-	MasterIP          string `json:"masterIP"`
-	MasterPort        int    `json:"masterPort"`
-	ClusterKey        string `json:"clusterKey"`
-	WorldFingerPrints string `json:"worldFingerPrints"`
+	Name           string `gorm:"primaryKey;not null" json:"name"`
+	DisplayName    string `json:"displayName"`
+	Status         bool   `json:"status"`
+	GameName       string `json:"gameName"`
+	Description    string `json:"description"`
+	GameMode       string `json:"gameMode"`
+	CustomGameMode string `json:"customGameMode"`
+	Pvp            bool   `json:"pvp"`
+	MaxPlayer      int    `json:"maxPlayer"`
+	MaxRollBack    int    `json:"maxRollBack"`
+	ModInOne       bool   `json:"modInOne"`
+	ModData        string `json:"modData"`
+	Vote           bool   `json:"vote"`
+	PauseEmpty     bool   `json:"pauseEmpty"`
+	Password       string `json:"password"`
+	Token          string `json:"token"`
+	MasterIP       string `json:"masterIP"`
+	MasterPort     int    `json:"masterPort"`
+	ClusterKey     string `json:"clusterKey"`
 }
 
 func (Room) TableName() string {
