f0578a72beac1bedd89ada5215b9957f535a8607
miracleEverywhere <763483966@qq.com>
2025-11-05 21:41:12 +0800

    20251105

--- dst/game.go
+++ dst/game.go
@@ -5,34 +5,67 @@ import (
 	"dst-management-platform-api/logger"
 	"dst-management-platform-api/utils"
 	"fmt"
+	"sync"
 )
 
 type Game struct {
 	room    *models.Room
 	worlds  *[]models.World
 	setting *models.RoomSetting
-	saveData
+	lang    string
+	roomSaveData
+	worldSaveData
 }
 
-type saveData struct {
+type roomSaveData struct {
+	// room 全局文件锁
+	roomMutex sync.Mutex
+	// dir
 	clusterName string
 	clusterPath string
+	// file
+	clusterIniPath      string
+	clusterTokenTxtPath string
 }
 
-func NewGameController(room *models.Room, worlds *[]models.World, setting *models.RoomSetting) *Game {
+type worldSaveData struct {
+	// world 全局文件锁
+	worldMutex sync.Mutex
+}
+
+func NewGameController(room *models.Room, worlds *[]models.World, setting *models.RoomSetting, lang string) *Game {
 	return &Game{
 		room:    room,
 		worlds:  worlds,
 		setting: setting,
+		lang:    lang,
 	}
 }
 
 func (g *Game) Save() error {
-	logger.Logger.Debug("哈哈哈哈哈哈哈")
+	g.initInfo()
+	logger.Logger.Debug(utils.StructToFlatString(g))
+
+	var err error
+
+	// cluster
+	err = g.createRoom()
+	if err != nil {
+		return err
+	}
+
+	// worlds
+	err = g.createWorlds()
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
 func (g *Game) initInfo() {
 	g.clusterName = fmt.Sprintf("Cluster_%d", g.room.ID)
 	g.clusterPath = fmt.Sprintf("%s/%s", utils.ClusterPath, g.clusterName)
+	g.clusterIniPath = fmt.Sprintf("%s/cluster.ini", g.clusterPath)
+	g.clusterTokenTxtPath = fmt.Sprintf("%s/cluster_token.txt", g.clusterPath)
 }
--- utils/system.go
+++ utils/system.go
@@ -21,3 +21,43 @@ func EnsureDirExists(dirPath string) error {
 
 	return nil
 }
+
+// EnsureFileExists 检查文件是否存在，如果不存在则创建空文件
+func EnsureFileExists(filePath string) error {
+	// 检查文件是否存在
+	_, err := os.Stat(filePath)
+	if os.IsNotExist(err) {
+		// 文件不存在，创建一个空文件
+		file, errCreate := os.Create(filePath)
+		if errCreate != nil {
+			return errCreate
+		}
+		errClose := file.Close()
+		if errClose != nil {
+			return errClose
+		}
+	} else if err != nil {
+		// 其他错误
+		return err
+	}
+
+	return nil
+}
+
+// TruncAndWriteFile 将指定内容完整写入文件，如果文件已存在会清空原有内容，如果文件不存在会创建新文件
+func TruncAndWriteFile(fileName string, fileContent string) error {
+	fileContentByte := []byte(fileContent)
+	file, err := os.OpenFile(fileName, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0666)
+	if err != nil {
+		return fmt.Errorf("打开或创建文件时出错: %w", err)
+	}
+	defer file.Close()
+
+	// 写入新数据
+	_, err = file.Write(fileContentByte)
+	if err != nil {
+		return fmt.Errorf("写入数据时出错: %w", err)
+	}
+
+	return nil
+}
