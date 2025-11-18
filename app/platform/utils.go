ddc7ab9f7b86c116480e5f89dc751bd7c5bc2162
miracleEverywhere <763483966@qq.com>
2025-11-18 16:49:53 +0800

    用户管理

--- app/platform/utils.go
+++ app/platform/utils.go
@@ -194,3 +194,8 @@ func getOSInfo() (*OSInfo, error) {
 		PlatformVersion: platformVersion,
 	}, nil
 }
+
+type Partition struct {
+	Page     int `json:"page" form:"page"`
+	PageSize int `json:"pageSize" form:"pageSize"`
+}
