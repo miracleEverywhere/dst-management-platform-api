0ecef384d17587686458788256efa8d962daf3f5
miracleEverywhere <763483966@qq.com>
2025-11-18 16:49:42 +0800

    控制面板

--- app/dashboard/utils.go
+++ app/dashboard/utils.go
@@ -1,15 +1,19 @@
 package dashboard
 
-import "dst-management-platform-api/database/dao"
+import (
+	"dst-management-platform-api/database/dao"
+)
 
 type Handler struct {
+	userDao        *dao.UserDAO
 	roomDao        *dao.RoomDAO
 	worldDao       *dao.WorldDAO
 	roomSettingDao *dao.RoomSettingDAO
 }
 
-func NewHandler(roomDao *dao.RoomDAO, worldDao *dao.WorldDAO, roomSettingDao *dao.RoomSettingDAO) *Handler {
+func NewHandler(userDao *dao.UserDAO, roomDao *dao.RoomDAO, worldDao *dao.WorldDAO, roomSettingDao *dao.RoomSettingDAO) *Handler {
 	return &Handler{
+		userDao:        userDao,
 		roomDao:        roomDao,
 		worldDao:       worldDao,
 		roomSettingDao: roomSettingDao,
