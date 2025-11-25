863063fe04ede68399052f2aaa47f7fdc6b05742
miracleEverywhere <763483966@qq.com>
2025-11-25 17:34:02 +0800

    一些控制面板的逻辑

--- app/dashboard/handler.go
+++ app/dashboard/handler.go
@@ -1,7 +1,156 @@
 package dashboard
 
-import "github.com/gin-gonic/gin"
+import (
+	"dst-management-platform-api/database/db"
+	"dst-management-platform-api/dst"
+	"dst-management-platform-api/logger"
+	"dst-management-platform-api/utils"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func (h *Handler) execGamePost(c *gin.Context) {
-	
+	type ReqForm struct {
+		Type    string `json:"type"`
+		RoomID  int    `json:"roomID"`
+		WorldID int    `json:"worldID"`
+		Extra   string `json:"extra"`
+	}
+
+	var reqForm ReqForm
+	if err := c.ShouldBindJSON(&reqForm); err != nil {
+		logger.Logger.Info("请求参数错误", "err", err, "api", c.Request.URL.Path)
+		c.JSON(http.StatusOK, gin.H{"code": 400, "message": message.Get(c, "bad request"), "data": nil})
+		return
+	}
+
+	room, worlds, roomSetting, err := h.fetchGameInfo(reqForm.RoomID)
+	if err != nil {
+		logger.Logger.Error("获取基本信息失败", "err", err)
+		c.JSON(http.StatusOK, gin.H{"code": 500, "message": message.Get(c, "database error"), "data": nil})
+		return
+	}
+
+	game := dst.NewGameController(room, worlds, roomSetting, c.Request.Header.Get("X-I18n-Lang"))
+
+	switch reqForm.Type {
+	case "startup":
+		// 启动
+		if reqForm.Extra == "all" {
+			err = game.StartAllWorld()
+			if err != nil {
+				logger.Logger.Error("启动失败", "err", err)
+				c.JSON(http.StatusOK, gin.H{"code": 201, "message": message.Get(c, "startup game fail"), "data": nil})
+				return
+			}
+		} else {
+			err = game.StartWorld(reqForm.WorldID)
+			if err != nil {
+				logger.Logger.Error("启动失败", "err", err)
+				c.JSON(http.StatusOK, gin.H{"code": 201, "message": message.Get(c, "startup game fail"), "data": nil})
+				return
+			}
+		}
+
+		c.JSON(http.StatusOK, gin.H{"code": 200, "message": message.Get(c, "startup game success"), "data": nil})
+		return
+	case "shutdown":
+		// 关闭
+		if reqForm.Extra == "all" {
+			err = game.StopAllWorld()
+			if err != nil {
+				logger.Logger.Error("关闭失败", "err", err)
+				c.JSON(http.StatusOK, gin.H{"code": 201, "message": message.Get(c, "shutdown game fail"), "data": nil})
+				return
+			}
+		} else {
+			err = game.StopWorld(reqForm.WorldID)
+			if err != nil {
+				logger.Logger.Error("关闭失败", "err", err)
+				c.JSON(http.StatusOK, gin.H{"code": 201, "message": message.Get(c, "shutdown game fail"), "data": nil})
+				return
+			}
+		}
+
+		c.JSON(http.StatusOK, gin.H{"code": 200, "message": message.Get(c, "shutdown game success"), "data": nil})
+		return
+	case "restart":
+		// 重启
+		_ = game.StopAllWorld()
+		err = game.StartAllWorld()
+		if err != nil {
+			logger.Logger.Error("启动失败", "err", err)
+			c.JSON(http.StatusOK, gin.H{"code": 201, "message": message.Get(c, "restart game fail"), "data": nil})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"code": 200, "message": message.Get(c, "restart game success"), "data": nil})
+		return
+	case "update":
+		// 更新，需要管理员权限
+		role, _ := c.Get("role")
+		if role.(string) != "admin" {
+			c.JSON(http.StatusOK, gin.H{"code": 201, "message": message.Get(c, "permission needed"), "data": nil})
+			return
+		}
+
+		go func() {
+			db.DstUpdating = true
+			updateCmd := fmt.Sprintf("cd ~/steamcmd && ./steamcmd.sh +login anonymous +force_install_dir ~/dst +app_update 343050 validate +quit")
+			_ = utils.BashCMD(updateCmd)
+			db.DstUpdating = false
+		}()
+
+		c.JSON(http.StatusOK, gin.H{"code": 200, "message": message.Get(c, "updating"), "data": nil})
+		return
+	case "reset":
+		if reqForm.Extra == "force" {
+			err = game.Reset(true)
+			if err != nil {
+				c.JSON(http.StatusOK, gin.H{"code": 201, "message": message.Get(c, "reset game fail"), "data": nil})
+				return
+			}
+		} else {
+			err = game.Reset(false)
+			if err != nil {
+				c.JSON(http.StatusOK, gin.H{"code": 201, "message": message.Get(c, "reset game fail"), "data": nil})
+				return
+			}
+		}
+
+		c.JSON(http.StatusOK, gin.H{"code": 200, "message": message.Get(c, "reset game success"), "data": nil})
+		return
+	case "delete":
+		err = game.DeleteWorld(reqForm.WorldID)
+		if err != nil {
+			c.JSON(http.StatusOK, gin.H{"code": 201, "message": message.Get(c, "delete game fail"), "data": nil})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"code": 200, "message": message.Get(c, "delete game success"), "data": nil})
+		return
+	case "announce":
+		err = game.Announce(reqForm.Extra)
+		if err != nil {
+			c.JSON(http.StatusOK, gin.H{"code": 201, "message": message.Get(c, "announce fail"), "data": nil})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"code": 200, "message": message.Get(c, "announce success"), "data": nil})
+		return
+	case "console":
+		err = game.ConsoleCmd(reqForm.Extra, reqForm.WorldID)
+		if err != nil {
+			c.JSON(http.StatusOK, gin.H{"code": 201, "message": message.Get(c, "exec fail"), "data": nil})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"code": 200, "message": message.Get(c, "exec success"), "data": nil})
+		return
+	default:
+		c.JSON(http.StatusOK, gin.H{"code": 400, "message": message.Get(c, "bad request"), "data": nil})
+		return
+	}
+
 }
--- app/dashboard/i18n.go
+++ app/dashboard/i18n.go
@@ -1,1 +1,62 @@
 package dashboard
+
+import "dst-management-platform-api/utils"
+
+type ExtendedI18n struct {
+	utils.BaseI18n
+}
+
+func NewExtendedI18n() *ExtendedI18n {
+	i := &ExtendedI18n{
+		BaseI18n: utils.BaseI18n{
+			ZH: make(map[string]string),
+			EN: make(map[string]string),
+		},
+	}
+
+	utils.I18nMutex.Lock()
+	defer utils.I18nMutex.Unlock()
+
+	for k, v := range utils.I18n.ZH {
+		i.ZH[k] = v
+	}
+	for k, v := range utils.I18n.EN {
+		i.EN[k] = v
+	}
+
+	i.ZH["startup game fail"] = "启动失败"
+	i.ZH["startup game success"] = "启动成功"
+	i.ZH["shutdown game fail"] = "关闭失败"
+	i.ZH["shutdown game success"] = "关闭成功"
+	i.ZH["restart game fail"] = "重启失败"
+	i.ZH["restart game success"] = "重启成功"
+	i.ZH["updating"] = "更新中，请耐心等待"
+	i.ZH["reset game fail"] = "重置失败"
+	i.ZH["reset game success"] = "重置成功"
+	i.ZH["delete game fail"] = "删除失败"
+	i.ZH["delete game success"] = "删除成功"
+	i.ZH["announce fail"] = "宣告失败"
+	i.ZH["announce success"] = "宣告成功"
+	i.ZH["exec fail"] = "执行失败"
+	i.ZH["exec success"] = "执行成功"
+
+	i.EN["startup game fail"] = "Startup Fail"
+	i.EN["startup game success"] = "Startup Success"
+	i.EN["shutdown game fail"] = "Shutdown Fail"
+	i.EN["shutdown game success"] = "Shutdown Success"
+	i.EN["restart game fail"] = "Restart Fail"
+	i.EN["restart game success"] = "Restart Success"
+	i.EN["updating"] = "Updating, please wait patiently"
+	i.EN["reset game fail"] = "Reset Fail"
+	i.EN["reset game success"] = "Reset Success"
+	i.EN["delete game fail"] = "Delete Fail"
+	i.EN["delete game success"] = "Delete Success"
+	i.EN["announce fail"] = "Announce Fail"
+	i.EN["announce success"] = "Announce Success"
+	i.EN["exec fail"] = "Execute Fail"
+	i.EN["exec success"] = "Execute Success"
+
+	return i
+}
+
+var message = NewExtendedI18n()
