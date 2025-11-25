aa0290abc43daa4ac49d81219ae8358bedff11b9
miracleEverywhere <763483966@qq.com>
2025-11-25 17:34:02 +0800

    一些控制面板的逻辑

--- app/dashboard/utils.go
+++ app/dashboard/utils.go
@@ -2,6 +2,7 @@ package dashboard
 
 import (
 	"dst-management-platform-api/database/dao"
+	"dst-management-platform-api/database/models"
 )
 
 type Handler struct {
@@ -18,4 +19,21 @@ func NewHandler(userDao *dao.UserDAO, roomDao *dao.RoomDAO, worldDao *dao.WorldD
 		worldDao:       worldDao,
 		roomSettingDao: roomSettingDao,
 	}
+}
+
+func (h *Handler) fetchGameInfo(roomID int) (*models.Room, *[]models.World, *models.RoomSetting, error) {
+	room, err := h.roomDao.GetRoomByID(roomID)
+	if err != nil {
+		return &models.Room{}, &[]models.World{}, &models.RoomSetting{}, err
+	}
+	worlds, err := h.worldDao.GetWorldsByRoomID(roomID)
+	if err != nil {
+		return &models.Room{}, &[]models.World{}, &models.RoomSetting{}, err
+	}
+	roomSetting, err := h.roomSettingDao.GetRoomSettingsByRoomID(roomID)
+	if err != nil {
+		return &models.Room{}, &[]models.World{}, &models.RoomSetting{}, err
+	}
+
+	return room, worlds, roomSetting, nil
 }
