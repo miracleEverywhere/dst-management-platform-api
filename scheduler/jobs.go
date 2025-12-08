8310b1749282ac48eb70b253db72d8e052c5f7d3
miracleEverywhere <763483966@qq.com>
2025-12-08 14:57:57 +0800

    添加定时任务

--- scheduler/jobs.go
+++ scheduler/jobs.go
@@ -66,5 +66,66 @@ func initJobs() {
 				})
 			}
 		}
+		// 备份清理 30
+		if roomSetting.BackupCleanEnable {
+			Jobs = append(Jobs, JobConfig{
+				Name:     fmt.Sprintf("%d-BackupClean", room.ID),
+				Func:     BackupClean,
+				Args:     []interface{}{room.ID, roomSetting.BackupCleanSetting},
+				TimeType: "day",
+				Interval: 0,
+				DayAt:    "05:16:27",
+			})
+		}
+		// 重启 "06:30:00"
+		if roomSetting.RestartEnable {
+			Jobs = append(Jobs, JobConfig{
+				Name:     fmt.Sprintf("%d-Restart", room.ID),
+				Func:     Restart,
+				Args:     []interface{}{game},
+				TimeType: "day",
+				Interval: 0,
+				DayAt:    roomSetting.RestartSetting,
+			})
+		}
+		// 自动开启关闭游戏 {"start":"07:00:00","stop":"01:00:00"}
+		if roomSetting.ScheduledStartStopEnable {
+			type ScheduledStartStopSetting struct {
+				Start string `json:"start"`
+				Stop  string `json:"stop"`
+			}
+			var scheduledStartStopSetting ScheduledStartStopSetting
+			if err := json.Unmarshal([]byte(roomSetting.ScheduledStartStopSetting), &scheduledStartStopSetting); err != nil {
+				logger.Logger.Error("获取自动开启关闭游戏设置失败", "err", err)
+				continue
+			}
+			Jobs = append(Jobs, JobConfig{
+				Name:     fmt.Sprintf("%d-ScheduledStart", room.ID),
+				Func:     ScheduledStart,
+				Args:     []interface{}{game},
+				TimeType: "day",
+				Interval: 0,
+				DayAt:    scheduledStartStopSetting.Start,
+			})
+			Jobs = append(Jobs, JobConfig{
+				Name:     fmt.Sprintf("%d-ScheduledStop", room.ID),
+				Func:     ScheduledStop,
+				Args:     []interface{}{game},
+				TimeType: "day",
+				Interval: 0,
+				DayAt:    scheduledStartStopSetting.Stop,
+			})
+		}
+		// 自动保活
+		if roomSetting.KeepaliveEnable {
+			Jobs = append(Jobs, JobConfig{
+				Name:     fmt.Sprintf("%d-Keepalive", room.ID),
+				Func:     Keepalive,
+				Args:     []interface{}{game, room.ID},
+				TimeType: "minute",
+				Interval: roomSetting.KeepaliveSetting,
+				DayAt:    "",
+			})
+		}
 	}
 }
