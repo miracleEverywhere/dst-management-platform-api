d605c991d1858b083747ecc00e93d11278e2b531
miracleEverywhere <763483966@qq.com>
2025-12-12 14:28:24 +0800

    修复字段错误

--- database/models/uidMap.go
+++ database/models/uidMap.go
@@ -1,7 +1,7 @@
 package models
 
 type UidMap struct {
-	UID      string `gorm:"primaryKey;not null;column:nickname" json:"uid"`
+	UID      string `gorm:"primaryKey;not null;column:uid" json:"uid"`
 	Nickname string `gorm:"not null;column:nickname" json:"nickname"`
 	RoomID   int    `gorm:"not null;column:room_id" json:"room_id"`
 }
