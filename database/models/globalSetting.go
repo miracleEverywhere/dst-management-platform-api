a1ff1069c6b0f8d5ac9f99d00a26da753046b886
miracleEverywhere <763483966@qq.com>
2025-12-12 14:28:32 +0800

    优化表结构

--- database/models/globalSetting.go
+++ database/models/globalSetting.go
@@ -4,7 +4,6 @@ type GlobalSetting struct {
 	ID                 int    `gorm:"primaryKey;not null;column:id" json:"id"`
 	PlayerGetFrequency int    `gorm:"column:player_get_frequency" json:"playerGetFrequency"`
 	UIDMaintainEnable  bool   `gorm:"column:uid_maintain_enable" json:"UIDMaintainEnable"`
-	UIDMaintainSetting int    `gorm:"column:uid_maintain_setting" json:"UIDMaintainSetting"`
 	SysMetricsEnable   bool   `gorm:"column:sys_metrics_enable" json:"sysMetricsEnable"`
 	SysMetricsSetting  int    `gorm:"column:sys_metrics_setting" json:"sysMetricsSetting"`
 	AutoUpdateEnable   bool   `gorm:"column:auto_update_enable" json:"autoUpdateEnable"`
