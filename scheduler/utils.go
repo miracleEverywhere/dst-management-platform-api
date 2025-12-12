7681252e37c7050a65e266ccd93e299a61759988
miracleEverywhere <763483966@qq.com>
2025-12-12 17:17:16 +0800

    重构游戏版本获取函数

--- scheduler/utils.go
+++ scheduler/utils.go
@@ -1,11 +1,17 @@
 package scheduler
 
 import (
+	"bufio"
 	"dst-management-platform-api/database/dao"
 	"dst-management-platform-api/database/models"
 	"dst-management-platform-api/logger"
+	"dst-management-platform-api/utils"
 	"fmt"
 	"github.com/go-co-op/gocron"
+	"io"
+	"net/http"
+	"os"
+	"strconv"
 	"sync"
 	"time"
 )
@@ -70,4 +76,100 @@ func fetchGameInfo(roomID int) (*models.Room, *[]models.World, *models.RoomSetti
 	}
 
 	return room, worlds, roomSetting, nil
+}
+
+type DSTVersion struct {
+	Local  int `json:"local"`
+	Server int `json:"server"`
+}
+
+func GetDSTVersion() DSTVersion {
+	var dstVersion DSTVersion
+	dstVersion.Server = 0
+	dstVersion.Local = 0
+
+	client := &http.Client{
+		Timeout: utils.HttpTimeout * time.Second,
+	}
+
+	file, err := os.Open(utils.DSTLocalVersionPath)
+	if err != nil {
+		logger.Logger.Error("获取游戏版本失败", "err", err)
+		return dstVersion
+	}
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			logger.Logger.Error("关闭文件失败", "err", err)
+		}
+	}(file) // 确保文件在函数结束时关闭
+
+	// 创建一个扫描器来读取文件内容
+	scanner := bufio.NewScanner(file)
+
+	// 扫描文件的第一行
+	if scanner.Scan() {
+		// 读取第一行的文本
+		line := scanner.Text()
+
+		// 将字符串转换为整数
+		number, err := strconv.Atoi(line)
+		if err != nil {
+			logger.Logger.Error("获取游戏版本失败", "err", err)
+			return dstVersion
+		}
+		dstVersion.Local = number
+		// 获取服务端版本
+		// 发送 HTTP GET 请求
+		response, err := client.Get(utils.DSTServerVersionApi)
+		if err != nil {
+			logger.Logger.Error("获取游戏版本失败", "err", err)
+			return dstVersion
+		}
+		defer func(Body io.ReadCloser) {
+			err := Body.Close()
+			if err != nil {
+				logger.Logger.Error("关闭文件失败", "err", err)
+			}
+		}(response.Body) // 确保在函数结束时关闭响应体
+
+		// 检查 HTTP 状态码
+		if response.StatusCode != http.StatusOK {
+			logger.Logger.Error("获取游戏版本失败", "err", err)
+			return dstVersion
+		}
+
+		// 读取响应体内容
+		body, err := io.ReadAll(response.Body)
+		if err != nil {
+			logger.Logger.Error("获取游戏版本失败", "err", err)
+			return dstVersion
+		}
+
+		// 将字节数组转换为字符串并返回
+		serverVersion, err := strconv.Atoi(string(body))
+		if err != nil {
+			logger.Logger.Error("获取游戏版本失败", "err", err)
+			return dstVersion
+		}
+
+		dstVersion.Server = serverVersion
+
+		return dstVersion
+	}
+
+	// 如果扫描器遇到错误，返回错误
+	if err := scanner.Err(); err != nil {
+		dstVersion.Server = 0
+		dstVersion.Local = 0
+		logger.Logger.Error("获取游戏版本失败", "err", err)
+
+		return dstVersion
+	}
+
+	// 如果文件为空，返回错误
+	dstVersion.Server = 0
+	dstVersion.Local = 0
+
+	return dstVersion
 }
