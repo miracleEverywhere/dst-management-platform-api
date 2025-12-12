5aed35a54a95ab707b4c289deb35efedb49b369a
miracleEverywhere <763483966@qq.com>
2025-12-12 14:28:57 +0800

    添加系统监控接口

--- app/platform/router.go
+++ app/platform/router.go
@@ -15,6 +15,7 @@ func (h *Handler) RegisterRoutes(r *gin.Engine) {
 			platform.GET("/game_version", middleware.MWtoken(), gameVersionGet)
 			platform.GET("/webssh", websshWS)
 			platform.GET("/os_info", middleware.MWtoken(), osInfoGet)
+			platform.GET("/metrics", middleware.MWtoken(), metricsGet)
 		}
 	}
 }
