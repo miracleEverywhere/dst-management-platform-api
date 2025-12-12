a07f8a7fad31dfb9831c1412feed9a0c23269ee0
miracleEverywhere <763483966@qq.com>
2025-12-12 14:27:58 +0800

    添加修改函数

--- database/dao/uidMap.go
+++ database/dao/uidMap.go
@@ -2,6 +2,7 @@ package dao
 
 import (
 	"dst-management-platform-api/database/models"
+	"fmt"
 	"gorm.io/gorm"
 )
 
@@ -15,9 +16,17 @@ func NewUidMapDAO(db *gorm.DB) *UidMapDAO {
 	}
 }
 
-func (d *UserDAO) GetUidMapByRoomID(roomID int) (*[]models.UidMap, error) {
+func (d *UidMapDAO) GetUidMapByRoomID(roomID int) (*[]models.UidMap, error) {
 	var uidMaps []models.UidMap
 	err := d.db.Where("room_id = ?", roomID).Find(&uidMaps).Error
 
 	return &uidMaps, err
+}
+
+func (d *UidMapDAO) UpdateUidMap(uidMap *models.UidMap) error {
+	if uidMap.UID == "" || uidMap.Nickname == "" || uidMap.RoomID == 0 {
+		return fmt.Errorf("三个字段不能为空")
+	}
+
+	return d.db.Save(uidMap).Error
 }
