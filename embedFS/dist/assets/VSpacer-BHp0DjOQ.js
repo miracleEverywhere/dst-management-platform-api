1581397b98b0e54be8196385b12ff851f667cbc0
miracleEverywhere <763483966@qq.com>
2025-12-17 20:43:15 +0800

    更新静态资源

--- embedFS/dist/assets/VSpacer-BHp0DjOQ.js
+++
@@ -1,5 +0,0 @@
-/* empty css              */
-import {ag as a} from "./index-CBjUexYG.js";
-
-const p = a("v-spacer", "div", "VSpacer");
-export {p as V};
--- embedFS/dist/assets/logs-Cl1JhOP1.js
+++
@@ -1,1 +0,0 @@
-import{h as n}from"./axios-KbiksPFU.js";const r={content:{url:"/logs/content",get:async function(t){return await n.get(this.url,t)}}};export{r as l};
