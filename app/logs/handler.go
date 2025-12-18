87a2099f4e8fb2348a9288c50768ebd0aea5b785
miracleEverywhere <763483966@qq.com>
2025-12-18 16:59:12 +0800

    优化日志获取

--- app/logs/handler.go
+++ app/logs/handler.go
@@ -3,6 +3,7 @@ package logs
 import (
 	"dst-management-platform-api/dst"
 	"dst-management-platform-api/logger"
+	"dst-management-platform-api/utils"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
@@ -21,21 +22,37 @@ func (h *Handler) contentGet(c *gin.Context) {
 		return
 	}
 
-	if reqForm.RoomID == 0 {
-		c.JSON(http.StatusOK, gin.H{"code": 400, "message": message.Get(c, "bad request"), "data": nil})
-		return
+	if reqForm.LogType == "game" || reqForm.LogType == "chat" {
+		if reqForm.RoomID == 0 {
+			c.JSON(http.StatusOK, gin.H{"code": 400, "message": message.Get(c, "bad request"), "data": nil})
+			return
+		}
+
+		room, worlds, roomSetting, err := h.fetchGameInfo(reqForm.RoomID)
+		if err != nil {
+			logger.Logger.Error("获取基本信息失败", "err", err)
+			c.JSON(http.StatusOK, gin.H{"code": 500, "message": message.Get(c, "database error"), "data": nil})
+			return
+		}
+
+		game := dst.NewGameController(room, worlds, roomSetting, c.Request.Header.Get("X-I18n-Lang"))
+
+		logContent := game.LogContent(reqForm.LogType, reqForm.WorldID, reqForm.Lines)
+
+		c.JSON(http.StatusOK, gin.H{"code": 200, "message": "success", "data": logContent})
+	} else {
+		var logPath string
+		switch reqForm.LogType {
+		case "steam":
+			logPath = "Steam/logs/bootstrap_log.txt"
+		case "access":
+			logPath = "logs/access.log"
+		case "runtime":
+			logPath = "logs/runtime.log"
+		}
+
+		logContent := utils.GetFileLastNLines(logPath, reqForm.Lines)
+		c.JSON(http.StatusOK, gin.H{"code": 200, "message": "success", "data": logContent})
 	}
 
-	room, worlds, roomSetting, err := h.fetchGameInfo(reqForm.RoomID)
-	if err != nil {
-		logger.Logger.Error("获取基本信息失败", "err", err)
-		c.JSON(http.StatusOK, gin.H{"code": 500, "message": message.Get(c, "database error"), "data": nil})
-		return
-	}
-
-	game := dst.NewGameController(room, worlds, roomSetting, c.Request.Header.Get("X-I18n-Lang"))
-
-	logContent := game.LogContent(reqForm.LogType, reqForm.WorldID, reqForm.Lines)
-
-	c.JSON(http.StatusOK, gin.H{"code": 200, "message": "success", "data": logContent})
 }
