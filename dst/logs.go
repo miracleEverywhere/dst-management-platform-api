d7c8b7ebe665be2eb20b7796908d893d51d368d0
miracleEverywhere <763483966@qq.com>
2025-12-18 16:59:12 +0800

    优化日志获取

--- dst/logs.go
+++ dst/logs.go
@@ -1,10 +1,9 @@
 package dst
 
 import (
-	"bufio"
 	"dst-management-platform-api/logger"
+	"dst-management-platform-api/utils"
 	"fmt"
-	"os"
 )
 
 func (g *Game) getLogContent(logType string, id, lines int) []string {
@@ -34,36 +33,5 @@ func (g *Game) getLogContent(logType string, id, lines int) []string {
 		return []string{}
 	}
 
-	return getLogFileLastNLines(logPath, lines)
-}
-
-func getLogFileLastNLines(filename string, n int) []string {
-	file, err := os.Open(filename)
-	if err != nil {
-		logger.Logger.Error("读取日志失败", "path", filename, "err", err)
-		return []string{}
-	}
-	defer func(file *os.File) {
-		err := file.Close()
-		if err != nil {
-			logger.Logger.Error("文件关闭失败", "err", err)
-		}
-	}(file)
-
-	var lines []string
-	scanner := bufio.NewScanner(file)
-
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-		if len(lines) > n {
-			lines = lines[1:] // 移除前面的行，保持最后 n 行
-		}
-	}
-
-	if err := scanner.Err(); err != nil {
-		logger.Logger.Error("读取日志失败", "path", filename, "err", err)
-		return []string{}
-	}
-
-	return lines
+	return utils.GetFileLastNLines(logPath, lines)
 }
