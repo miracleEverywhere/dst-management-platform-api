d904e40bccd8dd36fb911ffe4603797046fc581b
miracleEverywhere <763483966@qq.com>
2025-12-19 23:50:43 +0800

    数据库初始化重构

--- database/db/database.go
+++ database/db/database.go
@@ -2,14 +2,14 @@ package db
 
 import (
 	"dst-management-platform-api/database/models"
-	slog "dst-management-platform-api/logger"
+	"dst-management-platform-api/logger"
 	"dst-management-platform-api/utils"
 	"fmt"
 	"os"
 
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
+	dbLogger "gorm.io/gorm/logger"
 )
 
 var DB *gorm.DB
@@ -24,23 +24,23 @@ func InitDB() {
 
 	var err error
 	dsn := fmt.Sprintf("%s/dmp.db?cache=shared", utils.DBPath)
-	slog.Logger.Debug(fmt.Sprintf("数据库连接为%s", dsn))
+	logger.Logger.Debug(fmt.Sprintf("数据库连接为%s", dsn))
 
 	DB, err = gorm.Open(sqlite.Open(dsn), &gorm.Config{
-		Logger: logger.Default.LogMode(logger.Silent),
+		Logger: dbLogger.Default.LogMode(dbLogger.Silent),
 	})
 	if err != nil {
-		slog.Logger.Error("数据库连接失败", "err", err)
+		logger.Logger.Error("数据库连接失败", "err", err)
 		panic(fmt.Sprintf("数据库连接失败: %s", err.Error()))
 	}
 
-	slog.Logger.Info("数据库连接成功")
+	logger.Logger.Info("数据库连接成功")
 
 	CheckTables()
 }
 
 func CheckTables() {
-	slog.Logger.Debug("正在检查数据库表结构")
+	logger.Logger.Debug("正在检查数据库表结构")
 	err := DB.AutoMigrate(
 		&models.User{},
 		&models.System{},
@@ -51,8 +51,8 @@ func CheckTables() {
 		&models.UidMap{},
 	)
 	if err != nil {
-		slog.Logger.Error("数据库表结构检查失败", "err", err)
+		logger.Logger.Error("数据库表结构检查失败", "err", err)
 		panic(fmt.Sprintf("数据库表结构检查失败: %s", err.Error()))
 	}
-	slog.Logger.Debug("数据库表结构检查完成")
+	logger.Logger.Debug("数据库表结构检查完成")
 }
