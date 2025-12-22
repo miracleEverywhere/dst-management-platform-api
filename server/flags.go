b90db948f1f14339c527bd84089881b812fb3c77
miracleEverywhere <763483966@qq.com>
2025-12-22 13:23:12 +0800

    优化启动参数绑定

--- server/flags.go
+++ server/flags.go
@@ -5,16 +5,16 @@ import (
 )
 
 var (
-	BindPort    int
-	DBPath      string
-	LogLevel    string
-	VersionShow bool
+	bindPort    int
+	dbPath      string
+	logLevel    string
+	versionShow bool
 )
 
 func BindFlags() {
-	flag.IntVar(&BindPort, "bind", 80, "DMP端口, 如: -bind 8080")
-	flag.StringVar(&DBPath, "dbpath", "./data", "数据库文件目录, 如: -dbpath ./data")
-	flag.StringVar(&LogLevel, "level", "info", "日志等级, 如: -level debug")
-	flag.BoolVar(&VersionShow, "v", false, "查看版本，如： -v")
+	flag.IntVar(&bindPort, "bind", 80, "DMP端口, 如: -bind 8080")
+	flag.StringVar(&dbPath, "dbpath", "./data", "数据库文件目录, 如: -dbpath ./data")
+	flag.StringVar(&logLevel, "level", "info", "日志等级, 如: -level debug")
+	flag.BoolVar(&versionShow, "v", false, "查看版本，如： -v")
 	flag.Parse()
 }
