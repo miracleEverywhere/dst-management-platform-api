9fea86958d415d27596a795089fd26c15d2900f4
miracleEverywhere <763483966@qq.com>
2025-12-26 11:00:01 +0800

    优化日志下载返回

--- app/logs/handler.go
+++ app/logs/handler.go
@@ -176,14 +176,14 @@ func (h *Handler) downloadGet(c *gin.Context) {
 	var reqForm ReqForm
 	if err := c.ShouldBindQuery(&reqForm); err != nil {
 		logger.Logger.Info("请求参数错误", "err", err, "api", c.Request.URL.Path)
-		c.JSON(http.StatusOK, gin.H{"code": 400, "message": message.Get(c, "bad request"), "data": nil})
+		c.JSON(http.StatusBadRequest, gin.H{"code": 400, "message": message.Get(c, "bad request"), "data": nil})
 		return
 	}
 
 	room, worlds, roomSetting, err := h.fetchGameInfo(reqForm.RoomID)
 	if err != nil {
 		logger.Logger.Error("获取基本信息失败", "err", err)
-		c.JSON(http.StatusOK, gin.H{"code": 500, "message": message.Get(c, "database error"), "data": nil})
+		c.JSON(http.StatusInternalServerError, gin.H{"code": 500, "message": message.Get(c, "database error"), "data": nil})
 		return
 	}
 
@@ -204,7 +204,7 @@ func (h *Handler) downloadGet(c *gin.Context) {
 	err = utils.EnsureDirExists(zipFilePath)
 	if err != nil {
 		logger.Logger.Error("创建目录失败", "err", err)
-		c.JSON(http.StatusOK, gin.H{"code": 500, "message": message.Get(c, "download fail"), "data": nil})
+		c.JSON(http.StatusInternalServerError, gin.H{"code": 500, "message": message.Get(c, "download fail"), "data": nil})
 		return
 	}
 	zipFile := fmt.Sprintf("%s/logs.zip", zipFilePath)
@@ -212,7 +212,7 @@ func (h *Handler) downloadGet(c *gin.Context) {
 	err = utils.ZipFiles(fileList, zipFile)
 	if err != nil {
 		logger.Logger.Error("创建压缩文件失败", "err", err)
-		c.JSON(http.StatusOK, gin.H{"code": 500, "message": message.Get(c, "download fail"), "data": nil})
+		c.JSON(http.StatusInternalServerError, gin.H{"code": 500, "message": message.Get(c, "download fail"), "data": nil})
 		return
 	}
 
