c757d5d57226cb2c945b0f763e27f558ee72dff6
miracleEverywhere <763483966@qq.com>
2026-01-05 17:25:53 +0800

    优化安装脚本

--- embedFS/shell/manual_install.sh
+++ embedFS/shell/manual_install.sh
@@ -20,10 +20,11 @@ trap error_exit ERR
 function install_ubuntu() {
     dpkg --add-architecture i386
     apt update -y
+    apt install -y screen wget
     apt install -y lib32gcc1 || true
     apt install -y lib32gcc-s1 || true
-    apt install -y libcurl4-gnutls-dev:i386
-    apt install -y screen wget
+    apt install -y libcurl4-gnutls-dev:i386 || error_exit
+    apt install -y libcurl4-gnutls-dev || true
 }
 
 function install_rhel() {
@@ -31,6 +32,7 @@ function install_rhel() {
     yum -y install glibc.i686 libstdc++.i686 libcurl.i686
     yum -y install glibc libstdc++ libcurl
     yum -y install screen wget
+    ln -s /usr/lib/libcurl.so.4 /usr/lib/libcurl-gnutls.so.4
 }
 
 function check_screen() {
@@ -82,15 +84,23 @@ tar -zxvf steamcmd_linux.tar.gz -C "$STEAM_DIR"
 
 # 安装DST
 cd "$STEAM_DIR" || error_exit
+./steamcmd.sh +force_install_dir "$DST_DIR" +login anonymous +app_update 343050 validate +quit || true
+# 第一次安装dst可能会失败
 ./steamcmd.sh +force_install_dir "$DST_DIR" +login anonymous +app_update 343050 validate +quit
 
 # PR77 清理可能损坏的acf文件
 rm -rf "$DST_DIR/steamapps/appmanifest_343050.acf"
 
+# 一些必要的so文件
 cd "$HOME" || error_exit
 cp steamcmd/linux32/libstdc++.so.6 dst/bin/lib32/
-[ ! -L "dst/bin64/lib64/libcurl-gnutls.so.4" ] && ln -s /usr/lib64/libcurl.so.4 dst/bin64/lib64/libcurl-gnutls.so.4
-[ ! -L "dst/bin/lib32/libcurl-gnutls.so.4" ] && ln -s /usr/lib/libcurl.so.4 dst/bin/lib32/libcurl-gnutls.so.4
+if [[ "${OS}" == "ubuntu" || "${OS}" == "debian" ]]; then
+	[ ! -L "dst/bin64/lib64/libcurl-gnutls.so.4" ] && ln -s /usr/lib/x86_64-linux-gnu/libcurl-gnutls.so.4 dst/bin64/lib64/libcurl-gnutls.so.4
+	[ ! -L "dst/bin/lib32/libcurl-gnutls.so.4" ] && ln -s /usr/lib/i386-linux-gnu/libcurl-gnutls.so.4 dst/bin/lib32/libcurl-gnutls.so.4
+else
+	[ ! -L "dst/bin64/lib64/libcurl-gnutls.so.4" ] && ln -s /usr/lib64/libcurl.so.4 dst/bin64/lib64/libcurl-gnutls.so.4
+	[ ! -L "dst/bin/lib32/libcurl-gnutls.so.4" ] && ln -s /usr/lib/libcurl.so.4 dst/bin/lib32/libcurl-gnutls.so.4
+fi
 
 # luajit
 cd "$HOME" || error_exit
