31e5c9d69fe6102a6ec530ab531a25d92f4d20d9
miracleEverywhere <763483966@qq.com>
2026-07-07 17:48:58 +0800

    优化参数默认值

--- server/flags.go
+++ server/flags.go
@@ -16,7 +16,7 @@ var (
 
 func bindFlags() {
 	flag.IntVar(&bindPort, "bind", 80, "DMP端口, 如: -bind 8080")
-	flag.StringVar(&dbPath, "dbpath", "./data", "数据库文件目录, 如: -dbpath ./data")
+	flag.StringVar(&dbPath, "dbpath", "data", "数据库文件目录, 如: -dbpath ./data")
 	flag.StringVar(&logLevel, "level", "info", "日志等级, 如: -level debug")
 	flag.StringVar(&cert, "cert", "", "证书文件路径, 不填则启动http, 例如: /path/to/fullchain.pem")
 	flag.StringVar(&key, "key", "", "私钥文件路径, 不填则启动http, 例如: /path/to/privkey.pem")
